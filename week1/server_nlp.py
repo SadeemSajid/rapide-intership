c95a6660555242cc12d650912557d6fa6c385318
Sadeem Sajid <sadeemsajid.25@gmail.com>
2025-01-11 14:24:43 +0500

    feat: nlp server

--- week1/server_nlp.py
+++ week1/server_nlp.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
+# NLP library imports
+from transformers import pipeline
+
+# Generate pipeline
+transformer_pipe = pipeline("text-classification", model="bhadresh-savani/distilbert-base-uncased-emotion")
+
+# FASTAPI app
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# response format {result: [{label: x, score: y}]}
+@app.post("/analyse/{text}")
+async def analyse(text):
+	result = transformer_pipe(text)
+	return {"result": result }
